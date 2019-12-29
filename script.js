7d3691575df36d03e3b300f269d3c92853c2e4c4
Ari Horowitz <ariehh@comcast.net>
2019-12-28 17:22:45 -0800

    added more moment.js variables and console.log the result

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
 var format = "MMMM Do YYYY, h:mm:ss a";
 var result = moment().format(format);
 console.log(result);
+
+//moment.js past time
+var format = "";
+var result = moment()
+  .startOf("hour")
+  .fromNow();
+console.log(result);
+
+//moment.js future time
+var format = "";
+var result = moment()
+  .endOf("day")
+  .fromNow();
+console.log(result);
+
+//standard working hours
+var hours = [
+  "9:00am, 10:00am, 11:00am, 12,00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm"
+];
