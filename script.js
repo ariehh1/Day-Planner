af5a1cf853e43ce5d1fcfc0bbb527505ff35d40b
Ari Horowitz <ariehh@comcast.net>
2019-12-30 16:28:08 -0800

    on click save button return input field

--- script.js
+++ script.js
@@ -2,26 +2,42 @@
 // var dt = new Date();
 // document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
 
-//moment.js current date and time
-var format = "MMMM Do YYYY, h:mm:ss a";
-var result = moment().format(format);
-console.log(result);
+$(document).ready(function() {
+  $(".save-button").click(function() {
+    var inputValue = $(this)
+      .parent()
+      .parent()
+      .children("td")
+      .find("input")
+      .val();
+    console.log("clicked button");
+    console.log(inputValue);
+  });
+});
 
-//moment.js past time
-var format = "";
-var result = moment()
-  .startOf("hour")
-  .fromNow();
-console.log(result);
+// //moment.js current date and time
+// var format = "MMMM Do YYYY, h:mm:ss a";
+// var result = moment().format(format);
+// console.log(result);
 
-//moment.js future time
-var format = "";
-var result = moment()
-  .endOf("day")
-  .fromNow();
-console.log(result);
+// //moment.js past time
+// var format = "";
+// var result = moment()
+//   .startOf("hour")
+//   .fromNow();
+// console.log(result);
 
-//standard working hours
-var hours = [
-  "9:00am, 10:00am, 11:00am, 12,00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm"
-];
+// //moment.js future time
+// var format = "";
+// var result = moment()
+//   .endOf("day")
+//   .fromNow();
+// console.log(result);
+
+// //standard working hours
+// var hours = [
+//   "9:00am, 10:00am, 11:00am, 12,00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm"
+// ];
+
+// //when user clicks save
+// $onclick(save);
