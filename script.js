9ea98c75c1d1494d87a7bac65f1d7d84b6a1e204
Ari Horowitz <ariehh@comcast.net>
2020-01-03 15:40:22 -0800

    style table for thick border

--- script.js
+++ script.js
@@ -88,8 +88,3 @@ timeslotColor();
 //   .endOf("day")
 //   .fromNow();
 // console.log(result);
-
-// //standard working hours
-// var hours = [
-//   "9:00am, 10:00am, 11:00am, 12,00pm, 1:00pm, 2:00pm, 3:00pm, 4:00pm, 5:00pm"
-// ];
